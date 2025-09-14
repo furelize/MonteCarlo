749c0bf7e751c644d1dcecb9e78d515a8e4a7ce8
furelize <sebastian.fernstrom.warnholtz@malardalenstekniska.se>
2025-09-14 21:45:02 +0200

    Update MonteCarlo.c

--- MonteCarlo.c
+++ MonteCarlo.c
@@ -5,7 +5,23 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
-
 int main() {
 	printf("Welcome to the simulator\n");
+	printf("Do you want to use modern terminal mode or legacy gui mode, yes or no?\n")
+	char userchoiceinter[100];
+	fgets(userchoicegui, 10, stdin);
+	switch (userchoicegui) {
+		case (strcmp(userchoiceinter, yes)==0:
+			guiinterface()
+			break:
+		case (strcmp(userchoiceguinter, no)==0:
+			terminalinterface()
+			break:
+		default:
+		printf("please give a valid response");
+	}
+}
+void guiinterface() {
+}
+void terminainterface() {
 }
