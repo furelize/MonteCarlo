5e9f7e4aac074d8ab3c4b7f73a16ce832740a50f
Sebastian Warnholtz <sebastian.warnholtz@skola.botkyrka.se>
2025-10-03 19:05:03 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,32 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-class Start
+namespace Init
 {
-    static void Main()
+    class Start
     {
-        Console.Clear();
-        Console.WriteLine("Welcome to the simulator!");
-        Console.WriteLine("Please select the correct alternative");
-        Console.WriteLine("1. Run simulator");
-        Console.WriteLine("2. Credits");
-        Console.WriteLine("3. Exit");
-        string userchoicestr = Console.ReadLine();
-        while (userchoicestr != "Exit" && userchoicestr != "Simulator" && userchoicestr != "Credits")
+        static void Main()
         {
-            Console.WriteLine("Please give valid input");
-            userchoicestr = Console.ReadLine();
-        }
-        switch (userchoicestr)
-        {
-            case "Simulator":
-                //simulator();
-                break;
-            case "Credits":
-                //Credits();
-                break;
-            default:
-                Console.WriteLine("Thanks for using the program!");
-                break;
-        }
-        }
+            Console.Clear();
+            Console.WriteLine("Welcome to the simulator!");
+            Console.WriteLine("Please select the correct alternative");
+            Console.WriteLine("1. Simulator");
+            Console.WriteLine("2. Credits");
+            Console.WriteLine("3. Exit");
+            string userchoicestr = Console.ReadLine();
+            while (userchoicestr != "Exit" && userchoicestr != "Simulator" && userchoicestr != "Credits")
+            {
+                Console.WriteLine("Please give valid input");
+                userchoicestr = Console.ReadLine();
+            }
+            switch (userchoicestr)
+            {
+                case "Simulator":
+                    simulator();
+                    break;
+                case "Credits":
+                    Credits();
+                    break;
+                default:
+                    Console.WriteLine("Thanks for using the program!");
+                    break;
+            }
+            }
+            static void Credits()
+            {
+                Console.Clear();
+                Console.WriteLine("Written by Sebastian Warnholtz and Samuel Sylvan");
+            }
+            
+            static void simulator()
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the data file using ~ (standard linux filesystem is the only accepted format)");
+                string Datadir = Console.ReadLine();
+                while (File.Exists(Datadir))
+                {
+                    Console.WriteLine("Please link to a file that exists and that you have the correct permissions for");
+                    Datadir = Console.ReadLine();
+                }
+            }
+    }
 }
