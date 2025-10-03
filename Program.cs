005f146d14bf56041fdd4e5f461f8763dd50998d
Sebastian Warnholtz <sebastian.warnholtz@skola.botkyrka.se>
2025-10-03 17:39:29 +0200

    rewrite

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Start
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the simulator!");
+        Console.WriteLine("Please select the correct alternative");
+        Console.WriteLine("1. Run simulator");
+        Console.WriteLine("2. Credits");
+        Console.WriteLine("3. Exit");
+        string userchoicestr = Console.ReadLine();
+        while (userchoicestr != "Exit" && userchoicestr != "Simulator" && userchoicestr != "Credits")
+        {
+            Console.WriteLine("Please give valid input");
+            userchoicestr = Console.ReadLine();
+        }
+        switch (userchoicestr)
+        {
+            case "Simulator":
+                //simulator();
+                break;
+            case "Credits":
+                //Credits();
+                break;
+            default:
+                Console.WriteLine("Thanks for using the program!");
+                break;
+        }
+        }
+}
--- config.h
+++
@@ -1,2 +0,0 @@
-// settings for the program, it is in a different file for added complexity. 
-static bool coloroutput = false;
